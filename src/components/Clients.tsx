08a8b044c02eb2456f39d44669145c6227ed294f
andregustavo1 <andre@andregu.com.br>
2025-05-02 12:47:23 -0300

    up

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -26,7 +26,44 @@ const Clients = () => {
       observer.disconnect();
     };
   }, []);
-  const images = Array.from({ length: 26 }, (_, i) => `/img/1 (${i + 2}).jpg`);
+  const images = [
+    "/img/1 (2).jpg",
+    "/img/1 (3).jpg",
+    "/img/1 (35).jpg", // Coloque a imagem 35 em terceiro lugar
+    "/img/1 (4).jpg",
+    "/img/1 (34).jpg",
+    "/img/1 (5).jpg",
+    "/img/1 (33).jpg",
+    "/img/1 (6).jpg",
+    "/img/1 (32).jpg",
+    "/img/1 (7).jpg",
+    "/img/1 (8).jpg",
+    "/img/1 (31).jpg",
+    "/img/1 (9).jpg",
+    "/img/1 (30).jpg",
+    "/img/1 (10).jpg",
+    "/img/1 (29).jpg",
+    "/img/1 (11).jpg",
+    "/img/1 (28).jpg",
+    "/img/1 (12).jpg",
+    "/img/1 (13).jpg",
+    "/img/1 (14).jpg",
+    "/img/1 (15).jpg",
+    "/img/1 (16).jpg",
+    "/img/1 (17).jpg",
+    "/img/1 (18).jpg",
+    "/img/1 (19).jpg",
+    "/img/1 (20).jpg",
+    "/img/1 (21).jpg",
+    "/img/1 (22).jpg",
+    "/img/1 (23).jpg",
+    "/img/1 (24).jpg",
+    "/img/1 (25).jpg",
+    "/img/1 (26).jpg",
+    "/img/1 (27).jpg",
+    
+  ];
+
   return <section id="shows" ref={sectionRef} className="section-padding bg-gsx-black text-white">
       <div className="container mx-auto">
         <div className="text-center mb-14">
